c5adbd642adb5eadfd5c77786725a1149c830783
Andrey Kotelnikov <dobreutore@yandex.ru>
2023-07-14 11:57:04 +0300

    fix: gh-pages 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="react-mesto">
+    <BrowserRouter basename="/mesto-react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
